db563442a1471fb7a672304709058a6d88475d89
vengadesh31 <45032502+vengadesh31@users.noreply.github.com>
2018-11-19 05:50:55 +0530

    FirstHibernate

---
+++ FirstHibernate/src/com/jwt/hibernate/SimpleTest.java
@@ -0,0 +1,30 @@
+package com.jwt.hibernate;
+ 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+ 
+public class SimpleTest {
+ 
+    public static void main(String[] args) {
+ 
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+ 
+        SessionFactory factory = cfg.buildSessionFactory();
+        Session session = factory.openSession();
+        Student student = new Student();
+        student.setName("Mukesh");
+        student.setRoll("101");
+        student.setPhone("8888");
+        student.setDegree("B.E");
+ 
+        Transaction tx = session.beginTransaction();
+        session.save(student);
+        System.out.println("Object saved successfully.....!!");
+        tx.commit();
+        session.close();
+        factory.close();
+    }
+}
